a4f444e3930109e49224517ba9196f9eeb78c11c
wen <947018084@qq.com>
2021-11-17 17:33:45 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2021-11-17 17:25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+
+
+
+
+    }
+
+}
